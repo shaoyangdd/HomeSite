fda775c0ac42e227ba85137d9e45600c2f618d65
shaoyangdd <shaoyangdd@localhost>
2016-02-24 22:13:19 +0800

    去掉无用的引用

--- src/main/java/com/controller/CommetnAction.java
+++ src/main/java/com/controller/CommetnAction.java
@@ -2,18 +2,15 @@ package com.controller;
 
 import java.util.List;
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.entity.Comment;
 import com.service.CommentService;
 
